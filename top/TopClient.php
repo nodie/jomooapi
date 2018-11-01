0e699433c9c6380c46749ffcbc901f7f402076f8
lonny <1252959212@qq.com>
2018-11-01 16:56:47 +0800

    修改

--- top/TopClient.php
+++ top/TopClient.php
@@ -294,44 +294,48 @@ class TopClient
 
 				//--------------------------------字符串解析
 				$resp = json_decode($resp, true);
-				$resp = $resp['tmall_msf_reservation_response'];
-				if (isset($resp['result']) && $resp['result'] && !json_decode($resp['result'], true)) {
-					$str = preg_replace("/^{|}$/", "", $resp['result']);
-					$str = preg_replace("/, /", ",", $str);
-					$str = preg_replace("/: /", ":", $str);
-					$arr = explode(",", $str);
-					foreach ($arr as $key => $value) {
-						$r = explode("=", $value);
-						if (count($r) == 1) {
-							$r = explode(":", reset($r));
-						}
-
-						$r[0] = preg_replace("/^\"|\"$/", "", $r[0]);
-
-						if (isset($r[1])) {
-							$r[1] = preg_replace("/^\"|\"$/", "", $r[1]);
-
-							if ($r[1] == 'null') {
-								$r[1] = null;
-							} else if ($r[1] == 'true') {
-								$r[1] = true;
-							}  else if ($r[1] == 'false') {
-								$r[1] = false;
-							} else if (preg_match("/\d/", $r[1])) {
-								$r[1] = (int) $r[1];
+				if (isset($resp['tmall_msf_reservation_response'])) {
+					$resp = $resp['tmall_msf_reservation_response'];
+					if (isset($resp['result']) && $resp['result'] && !json_decode($resp['result'], true)) {
+						$str = preg_replace("/^{|}$/", "", $resp['result']);
+						$str = preg_replace("/, /", ",", $str);
+						$str = preg_replace("/: /", ":", $str);
+						$arr = explode(",", $str);
+						foreach ($arr as $key => $value) {
+							$r = explode("=", $value);
+							if (count($r) == 1) {
+								$r = explode(":", reset($r));
 							}
+
+							$r[0] = preg_replace("/^\"|\"$/", "", $r[0]);
+
+							if (isset($r[1])) {
+								$r[1] = preg_replace("/^\"|\"$/", "", $r[1]);
+
+								if ($r[1] == 'null') {
+									$r[1] = null;
+								} else if ($r[1] == 'true') {
+									$r[1] = true;
+								}  else if ($r[1] == 'false') {
+									$r[1] = false;
+								} else if (preg_match("/\d/", $r[1])) {
+									$r[1] = (int) $r[1];
+								}
+							}
+
+							$arr[$r[0]] = $r[1];
+
+							unset($arr[$key]);
 						}
 
-						$arr[$r[0]] = $r[1];
-
-						unset($arr[$key]);
+						$resp['result'] = $arr;
 					}
+					$resp = array('tmall_msf_reservation_response' => $resp);
 
-					$resp['result'] = $arr;
+					$resp = json_encode($resp);
+				} else {
+					$resp = json_encode($resp);
 				}
-				$resp = array('tmall_msf_reservation_response' => $resp);
-
-				$resp = json_encode($resp);
 				//--------------------------------字符串解析
 			}
 		}
